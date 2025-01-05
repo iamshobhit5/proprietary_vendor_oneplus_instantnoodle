f54a4f628b29c89f934dbaaeddabaa0a6634f4c4
Shobhit Singh <iam@shobhit.win>
2025-01-05 19:21:24 +0530

    instantnoodle: Add bu52053nvx sensor config Change-Id: I984bb04fdf9087602fc7f770f8b0a8fba2c09f9a

--- instantnoodle-vendor.mk
+++ instantnoodle-vendor.mk
@@ -44,6 +44,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/instantnoodle/proprietary/odm/etc/irissoft.fw:$(TARGET_COPY_OUT_ODM)/etc/irissoft.fw \
     vendor/oneplus/instantnoodle/proprietary/odm/etc/sensor/config/kona_ak991x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_ak991x_0.json \
     vendor/oneplus/instantnoodle/proprietary/odm/etc/sensor/config/kona_bmi26x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_bmi26x_0.json \
+    vendor/oneplus/instantnoodle/proprietary/odm/etc/sensor/config/kona_bu52053nvx_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_bu52053nvx_0.json \
     vendor/oneplus/instantnoodle/proprietary/odm/etc/sensor/config/kona_mmc5603x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_mmc5603x_0.json \
     vendor/oneplus/instantnoodle/proprietary/odm/etc/sensor/config/kona_qrd_alsps.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_alsps.json \
     vendor/oneplus/instantnoodle/proprietary/odm/etc/sensor/config/kona_qrd_bmi26x_0.json:$(TARGET_COPY_OUT_ODM)/etc/sensor/config/kona_qrd_bmi26x_0.json \
